0b2da4013df62e18cae7354148399d5e0a1badb1
Samuel Santos <135053698+sabsfilho@users.noreply.github.com>
2024-02-26 03:49:27 +0000

    webapi mvc lab01

--- docs/rebrand/ASPNET/WebAPI/WebMVCLab01/Pages/Index.cshtml.cs
+++ docs/rebrand/ASPNET/WebAPI/WebMVCLab01/Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ namespace WebMVCLab01.Pages;
 
 public class IndexModel : PageModel
 {
+    public string Message { get; private set; } = "PageModel in C#";
     private readonly ILogger<IndexModel> _logger;
 
     public IndexModel(ILogger<IndexModel> logger)
@@ -14,6 +15,6 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
-
+        Message += $" Server time is { DateTime.Now }";
     }
 }
