79ce2d657f2498d146d11c1bdb41921187213919
Samuel Santos <135053698+sabsfilho@users.noreply.github.com>
2024-02-26 18:38:45 +0000

    SearchKeyInSortedMatrix dsa

--- docs/rebrand/DSA/DSAConsole/Array_LeetCode_SearchKeyInSortedMatrix.cs
+++ docs/rebrand/DSA/DSAConsole/Array_LeetCode_SearchKeyInSortedMatrix.cs
@@ -1,6 +1,42 @@
 class Array_LeetCode_SearchKeyInSortedMatrix{
+    
+    private static (int i, int j) Search(int[,] xs, int n, int k) {
+        int i = 0;
+        int j = n - 1;
+        while (i < n && j >= 0){
+            int x = xs[i, j];
+            if (x == k){
+                return (i, j);
+            }
+            if (x > k){
+                j--;
+            }
+            else {
+                i++;
+            }
+        }
+        return (-1, -1);
+    }
+
     public static void Print(){
         Console.WriteLine("Array_LeetCode_SearchKeyInSortedMatrix");
-        
+
+        int[,] xs = {
+            {10,20,30,40},
+            {15,25,35,45},
+            {27,29,37,48},
+            {32,33,39,51}
+        };
+
+        int k = 36;
+
+        int n = xs.GetUpperBound(0) + 1;
+
+        var p = Search(xs, n, k);
+
+        Console.WriteLine(
+            p.i == -1 ? $"{k} not found" :
+            $"{k} found at ({p.i}, {p.j})"
+        );        
     }
 }
