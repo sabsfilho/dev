66787f18285e9efe424d7c08231423764320db41
Samuel Santos <135053698+sabsfilho@users.noreply.github.com>
2024-02-29 02:10:29 +0000

    asp.net webapi mvc

--- docs/rebrand/ASPNET/WebAPI/WebAPIMVCLab01/Models/WeatherForecast.cs
+++ docs/rebrand/ASPNET/WebAPI/WebAPIMVCLab01/Models/WeatherForecast.cs
@@ -1,8 +1,8 @@
 
     public class WeatherForecast
     {
-        public DateTime Date;
-        public int TemperatureC;
-        public string? Summary;
+        public DateTime Date { get; set; }
+        public int TemperatureC { get; set; }
+        public string? Summary { get; set; }
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
     }
--- docs/rebrand/ASPNET/WebAPI/WebAPIMVCLab01/wwwroot/js/site.js
+++ docs/rebrand/ASPNET/WebAPI/WebAPIMVCLab01/wwwroot/js/site.js
@@ -2,3 +2,35 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const CtCtl = function() {
+
+    const addButton = (act, method, nm, o) => {
+        var btn = document.createElement('button');
+        btn.innerText = nm;
+        btn.onclick = () => {
+            fetch(act, {
+                method: method,
+                body: JSON.stringify(o),
+                headers: {"Content-type": "application/json; charset=UTF-8"}
+              })
+              .then(response => response.json()) 
+              .then(json => console.log(json))
+              .catch(err => console.log(err))
+        };
+        document.getElementsByTagName('body')[0].appendChild(btn);
+
+    }
+
+    const load = ()=>{
+        addButton('/Pizza', 'GET', 'GET Pizzas');
+        addButton('/Pizza', 'POST', 'POST Pizza', {"name":"Calab","isGlutenFree":false});
+        addButton('/Pizza/3', 'PUT', 'PUT Pizza', {"id":"3", "name":"Calabresa"});
+        addButton('/Pizza/2', 'DELETE', 'DELETE Pizza', {});
+    };
+
+    return {
+        load:load
+    }
+}();
+
+window.onload = CtCtl.load;
