98a13fae1ce62e06f57363f0bbcc78b4b533f36d
Samuel Santos <135053698+sabsfilho@users.noreply.github.com>
2024-03-09 02:27:15 +0000

    exercise tracker url fix

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -133,7 +133,7 @@ by tick giving time to the trader evaluate better strategies for his portfolio.<
                 tit: 'Back End, npm, Node.js, Express.js, Mongoose.js, AZURE',
                 k: 'rebrand/FreeCodeCamp/Certification/BackEndDevelopment',
                 projs: [
-                    {u:'https://boilerplate-project-mongodb.azurewebsites.net/',n:'Exercise Tracker',tags:'Node.js,Express.js,Mongoose.js,MONGODB,AZURE',d:''},
+                    {u:'https://lab-project-01.azurewebsites.net/api/exercisetracker',n:'Exercise Tracker',tags:'Node.js,Express.js,Mongoose.js,MONGODB,AZURE',d:''},
                     {u:'https://lab-project-01.azurewebsites.net/api/express',n:'Express Microservice',tags:'Node.js,Express.js,AZURE',d:''},
                     {u:'https://lab-project-01.azurewebsites.net/api/filemetadata.',n:'File Metadata',tags:'Node.js,Express.js,multer.js,AZURE',d:''},
                     {u:'https://lab-project-01.azurewebsites.net/api/headerparser.',n:'Header Parser',tags:'Node.js,Express.js,AZURE',d:''},
