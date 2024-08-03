d947d26d5427960d083931e8143497855474fc61
Samuel Santos <sabsfilho@gmail.com>
2024-08-03 02:02:49 -0300

    create-aws-lambda-user-roles review

--- docs/rebrand/aws/create-aws-lambda-user-roles.sh
+++ docs/rebrand/aws/create-aws-lambda-user-roles.sh
@@ -7,8 +7,8 @@
 #
 #This script will:
 # - Create Group named LambdaGroup
-# - Create User named LambdaUser
-# - Apply FullAccessLambda role to LambdaGroup
+# - Create User named LambdaUser adding it to LambdaGroup
+# - Apply FullAccessLambda and ECR_AllowAuthorization roles to LambdaGroup
 # - Create Role DockerHeroRole
 # - Apply AWSLambdaBasicExecutionRole role to DockerHeroRole
 
@@ -22,10 +22,10 @@ echo Add LambdaUser to LambdaGroup
 aws iam add-user-to-group --user-name LambdaUser --group-name LambdaGroup
 
 echo Apply FullAccessLambda to LambdaGroup
-aws iam attach-role-policy --role-name LambdaGroup --policy-arn arn:aws:iam::aws:policy/AWSLambda_FullAccess
+aws iam attach-group-policy --group-name LambdaGroup --policy-arn arn:aws:iam::aws:policy/AWSLambda_FullAccess
 
 echo Apply ECR_AllowAuthorization to LambdaGroup
-aws iam attach-role-policy --role-name LambdaGroup --policy-arn arn:aws:iam::091201685298:policy/ECR_AllowAuthorization
+aws iam attach-group-policy --group-name LambdaGroup --policy-arn arn:aws:iam::091201685298:policy/ECR_AllowAuthorization
 
 echo Create DockerHeroRole
 aws iam create-role --role-name DockerHeroRole --assume-role-policy-document '{"Version": "2012-10-17","Statement": [{ "Effect": "Allow", "Principal": {"Service": "lambda.amazonaws.com"}, "Action": "sts:AssumeRole"}]}'
@@ -36,7 +36,6 @@ aws iam attach-role-policy --role-name DockerHeroRole --policy-arn arn:aws:iam::
 echo Create Access Key
 aws iam create-access-key --user-name LambdaUser
 
-echo "This script was executed successfully."
 echo "You can use the LambdaUser credentials AccessKeyId and SecretAccessKey to consume AWS Lambda Function resources."
 
 echo -e "\n~~ END ~~\n"
