8096ca5810f85077b8f47f597d5a72a2de1f9cfd
Samuel Santos <sabsfilho@gmail.com>
2024-08-27 02:42:52 -0300

    soa high level design

---
+++ docs/rebrand/DSA/Leet75_202408/MonotonicStack/901-online-stock-span.cs
@@ -0,0 +1,27 @@
+﻿public class StockSpanner {
+
+    List<int> stack = null;
+
+    public StockSpanner() {
+        stack = new List<int>();
+    }
+    
+    public int Next(int price) {
+        stack.Add(price);
+        //Console.WriteLine(price);
+        //Console.WriteLine(string.Join(",", stack));
+        int q = 0;
+        for(int i = stack.Count - 1; i >= 0; i--) {
+            int v = stack[i];
+            if (v > price) return q;
+            q++;
+        }
+        return q;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.Next(price);
+ */
