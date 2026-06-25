65a99043b0368d2087ff85dfdec15151b2e3dede
Samuel Santos <samuel.santos.engineer@gmail.com>
2026-06-25 01:21:49 -0300

    add TaumandBdayResult

---
+++ docs/rebrand/HackerRank/easy/TaumandBdayResult.cs
@@ -0,0 +1,75 @@
+// Taum and B'day
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class TaumandBdayResult
+{
+
+    /*
+     * Complete the 'taumBday' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER b
+     *  2. INTEGER w
+     *  3. INTEGER bc
+     *  4. INTEGER wc
+     *  5. INTEGER z
+     */
+
+    public static long taumBday(int b, int w, int bc, int wc, int z)
+    {
+        long bk = Math.Min(bc, wc + z);
+        long wk = Math.Min(wc, bc + z);
+        long bt = b * bk;
+        long wt = w * wk;
+        
+        return bt + wt;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int b = Convert.ToInt32(firstMultipleInput[0]);
+
+            int w = Convert.ToInt32(firstMultipleInput[1]);
+
+            string[] secondMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int bc = Convert.ToInt32(secondMultipleInput[0]);
+
+            int wc = Convert.ToInt32(secondMultipleInput[1]);
+
+            int z = Convert.ToInt32(secondMultipleInput[2]);
+
+            long result = Result.taumBday(b, w, bc, wc, z);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
